33002e8a2d0c137f8632815b8ecd2dc930ac8015
Yaeeun Won <wyeyc12@gmail.com>
2022-03-17 23:00:19 +0900

    백준 11050  이향계수 1번 문제 풀이 파이썬

--- codepractice/codepractice.c
+++ codepractice/codepractice.c
@@ -1,13 +1,6 @@
 ﻿
-
-#파이썬으로 바꾸려고 하는 데 안되네. 백준 1149 rgb거리
-n = int(input())
-p = []
-for i in range(n) :
-    p.append(list(map(int, input().split())))
-    for i in range(1, len(p)) :
-        p[i][0] = min(p[i - 1][1], p[i - 1][2]) + p[i][0]
-        p[i][1] = min(p[i - 1][0], p[i - 1][2]) + p[i][1]
-        p[i][2] = min(p[i - 1][0], p[i - 1][1]) + p[i][2]
-        print(min(p[n - 1][0], p[n - 1][1], p[n - 1][2]))
-
+#파이썬으로 연결하는 거 배워야 돼!
+from math import factorial
+n, k = map(int, input().split())
+b = factorial(n) // (factorial(k) * factorial(n - k))
+print(b)

6175aa2146968453bb864620e9abb729c1593377
Yaeeun Won <wyeyc12@gmail.com>
2022-05-31 23:33:18 +0900

    백준  22857

--- codepractice/codepractice.c
+++ codepractice/codepractice.c
@@ -11,5 +11,39 @@ c = list(map(int, input().split()))
 for i in c :
 if i < b :
     print(i, end = " ")
-    잘살고싶다 성공하고싶다
-    tnf whgdk
+  
+
+
+#220531
+import sys
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+lst = list(map(int, input().split()))
+
+cnt = 0
+start, end = 0, 0
+size, size_max = 0, 0
+flag = 1
+
+for start in range(n) :
+    while cnt <= k and flag :
+        if lst[end] % 2 :
+            if cnt == k :
+                break
+                cnt += 1
+                size += 1
+                if end == n - 1 :
+                    flag = 0
+                    break
+                    end += 1
+
+                    if size_max < size - cnt :
+                        size_max = size - cnt
+
+                        if lst[start] % 2 :
+                            cnt -= 1
+
+                            size -= 1
+
+                            print(size_max)

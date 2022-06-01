6a949cd738b939544620ad6569801933f17fa066
Yaeeun Won <wyeyc12@gmail.com>
2022-06-01 23:38:30 +0900

    2212 센서

--- codepractice/codepractice.c
+++ codepractice/codepractice.c
@@ -47,3 +47,21 @@ for start in range(n) :
                             size -= 1
 
                             print(size_max)
+
+
+
+
+
+#220601
+n = int(input())
+k = int(input())
+sensor = list(map(int, input().split()))
+sensor.sort()
+btwss = [sensor[i] - sensor[i - 1] for i in range(1, n)]
+btwss.sort()
+
+if k >= n : print(0)
+else : #k < n
+    for i in range(k - 1) :
+        btwss.pop()
+        print(sum(btwss))

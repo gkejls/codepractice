f0323999499c55fb185fd2bc84ffbbba57f384d3
Yaeeun Won <wyeyc12@gmail.com>
2022-06-03 22:02:43 +0900

    백준 알고리즘 14720번 우유 축제

--- codepractice/codepractice.c
+++ codepractice/codepractice.c
@@ -96,3 +96,13 @@ if (1 <= n <= N) and check[n] == -1 :
             graph[u].append(v)
             graph[v].append(u)
             cnt = bfs(S)
+
+#
+            N = int(input())
+            li = list(map(int, input().split()))
+            k = cnt = 0
+            for i in range(li.index(0), N) :
+                if li[i] == k :
+                    k = (k + 1) % 3
+                    cnt += 1
+                    print(cnt)

beaa602b5b80bcb9887761477ff75fa74708c298
Yaeeun Won <wyeyc12@gmail.com>
2022-08-24 23:48:10 +0900

    내일 일찍 일어나자

--- codepractice/codepractice.c
+++ codepractice/codepractice.c
@@ -75,7 +75,7 @@ def bfs(node) :
     q = deque()
         q.append(node)
         check[node] = 0
-        while q :  ssssssss
+        while q :  ssssssss gmrgmr
             node = q.popleft()
             d = [node - 1, node + 1]
             if graph[node] :

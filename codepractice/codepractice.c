4fa6343de033e43a6e34f4248c8c7a02c2133e21
Yaeeun Won <wyeyc12@gmail.com>
2022-08-30 23:34:38 +0900

    가장 긴 행을 출력하는 프로그램

--- codepractice/codepractice.c
+++ codepractice/codepractice.c
@@ -3,22 +3,27 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int power(int m, int n);
+/*count digits, white space, nothers */
+#define MAXLINE 1000 /*maximum input line size*/
 
+int getline(char line[], int maxline);
+void copy(char to[], char from[]);
+
+/*print longest input line*/
 int main(int argc, char* argv[]) {
-	#0827
-		int i;
-	for (i = 0; i < 10; i++)
-		printf("%d %d %d\n", i, power(2, i), power(-3, i));
+	#0830
+		int len; 				/* current line length */
+	int max;				/* maximum length so far */
+	char line[MAXLINE]; 	/* current input line */
+	char longest[MAXLINE]; /* longest line saved here */
+
+	max = 0;
+	while ((len = getline(line, MAXLINE)) > 0) //??
+		if (len > max) {
+			mam = len;
+			copy(longest, line);
+		}
 	return 0;
 }
 
-int power(int base, int n)
-{
-	int i, p;
-	int i, p;
-	p = 1;
-	for (i = 1; i <= n; i++)
-		p = p * base;
-	return p;
-}
+//c언언데 설정하는 거 너무 귀찮아서 그동안 안 만짐..

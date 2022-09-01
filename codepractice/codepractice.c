77ca224959dfd3c2ff19dc4c9b26ebad57b42531
Yaeeun Won <wyeyc12@gmail.com>
2022-09-01 22:09:25 +0900

    최대수익을 구하는 알고리즘

--- codepractice/codepractice.c
+++ codepractice/codepractice.c
@@ -1,19 +1,45 @@
-﻿
-def fact(n) :
-
-    f = 1                      # 곱을 계산할 변수, 초깃값은 1
-
-    for i in range(1, n + 1) : # 1부터 n까지 반복(n + 1은 제외)
-
-        f = f * i              # 곱셈 연산으로 수정
-
-        return f
+﻿# 주어진 주식 가격을 보고 얻을 수 있는 최대 수익을 구하는 알고리즘
+# 입력: 주식 가격의 변화 값(리스트 : prices)
+# 출력 : 한 주를 한 번 사고팔아 얻을 수 있는 최대 수익 값
+# 1번을 해보세요!
 
 
+def max_profit(prices) :
 
-        print(fact(1))                 # 1!= 1
+    n = len(prices)
 
-        print(fact(5))                 # 5!= 120
+    max_profit = 0         # 최대 수익은 항상 0 이상의 값
 
-        print(fact(10))                # 10!= 3628800
+    min_price = prices[0]  # 첫째 날의 주가를 주가의 최솟값으로 기억
 
+    for i in range(1, n) : # 1부터 n - 1까지 반복
+
+        # 지금까지의 최솟값에 주식을 사서 i날에 팔 때의 수익
+
+        profit = prices[i] - min_price
+
+        if profit > max_profit:    # 이 수익이 지금까지 최대 수익보다 크면 값을 고침
+
+            max_profit = profit
+
+            if prices[i] < min_price:  # i날 주가가 최솟값보다 작으면 값을 고침
+
+                min_price = prices[i]
+
+
+
+                return max_profit
+
+
+
+
+
+
+
+
+
+
+
+
+                stock = [10300, 9600, 9800, 8200, 7800, 8300, 9500, 9800, 10200, 9500]
+                print(max_profit(stock))

1a44c3f09a1fd61ec437488bba03aff7a6b718b9
Yaeeun Won <wyeyc12@gmail.com>
2022-09-08 21:28:19 +0900

    아날로그 led 밝기 조절

--- codepractice/codepractice.c
+++ codepractice/codepractice.c
@@ -1,6 +1,16 @@
-﻿pinMode(13, OUTPUT);
-//디지털 핀 13반을 아웃풋 핀으로 설정하는 함수
-digitalWrite(13, HIGH)
-//디지털입력할 때 13번 기본 전압을 하이로 
-analogRead(A0);
-//아날로그 AO핀을 읽는 입력문?
+﻿void setup() {
+    // put your setup code here, to run once:
+
+}
+
+void loop() {
+    // put your main code here, to run repeatedly:
+    for (int i = 0; i < 256; i++) {
+        analogWrite(9, i);
+        delay(10);
+    }
+    for (int i = 255; i > -1; i--) {
+        analogWrite(9, i);
+        delay(10);
+    }
+}

99973bceb853cd954474c7e0efa22262c884291b
Yaeeun Won <wyeyc12@gmail.com>
2022-09-13 23:34:40 +0900

    백준 날짜계산

--- codepractice/codepractice.c
+++ codepractice/codepractice.c
@@ -1,15 +1,17 @@
-﻿void setup() {
-	// put your setup code here, to run once:
-	// 아날로그라 설정할 거 없음
-	Serial.begin(9600);
-}
+﻿#include <iostream>
+using namespace std;
 
-void loop() {
-	// put your main code here, to run repeatedly:
-	int light = analogRead(A0);
-	int ledLight = map(light, 0, 1023, 255, 0);
-	analogWrite(9, ledLight);
-	Serial.print("light is ");
-	Serial.println(ledLight);
-	delay(300);
-} //again//
+int main() {
+	int E, S, M = 0;
+	cin >> E >> S >> M;
+
+	int idx = 0;
+	int year = 0;
+	while (1) {
+		if ((year % 15) + 1 == E && (year % 28) + 1 == S && (year % 19) + 1 == M) {
+			cout << year + 1;
+			break;
+		}
+		else year++;
+	}
+}

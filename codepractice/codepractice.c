c4ef20890706850282df92b6b8f83999ba0a3582
Yaeeun Won <wyeyc12@gmail.com>
2022-09-18 23:29:52 +0900

    const?

--- codepractice/codepractice.c
+++ codepractice/codepractice.c
@@ -1,52 +1,16 @@
-﻿#include <iostream>
-#include <vector>
-#include <list>
+﻿
+const char trigpin = 13;
+const char echopin = 12;
 
-using namespace std;
+//펄스 폭과 거리 변수 설정
+int pulseWidth;
+int distance;
+int distanceOld;
 
-int main()
-{
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-
-	list<int> josephus;
-	list<int>::iterator it;
-	vector<int> answer;
-
-	int n, k;
-	cin >> n >> k;
-
-	// 연결 리스트 초기화
-	for (int i = 1; i <= n; i++) {
-		josephus.push_back(i);
-	}
-
-	it = josephus.begin();
-
-	// 요세푸스 순열 생성
-	while (!josephus.empty()) {
-		for (int i = 0; i < k - 1; i++) {
-			it++;
-
-			// 맨 끝에 도달했으면 다시 처음으로 돌아감
-			if (it == josephus.end()) {
-				it = josephus.begin();
-			}
-		}
-
-		answer.push_back(*it);
-		it = josephus.erase(it);
-
-		// 맨 끝에 도달했으면 다시 처음으로 돌아감
-		if (it == josephus.end()) {
-			it = josephus.begin();
-		}
-	}
-
-	// 요세푸스 순열 출력
-	cout << "<";
-	for (int i = 0; i < n; i++) cout << answer[i] << (i < n - 1 ? ", " : "");
-	cout << ">\n";
-
-	return 0;
+void setup() {
+	Serial.begin(9600);
+	pinMode(trigPin, OUTPUT);
+	pinMode(echoPin, INPUT);
+	//트리거 핀의 초기값을 LOW로 설정
+	digitalWrite(trigPin, LOW);
 }

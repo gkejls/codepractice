ecf04621e02553fa4043a09b8e84cf141e9a8418
Yaeeun Won <wyeyc12@gmail.com>
2022-09-25 22:48:05 +0900

    영역구하기 c++

--- codepractice/codepractice.c
+++ codepractice/codepractice.c
@@ -1,102 +1,59 @@
-﻿#include<bits/stdc++.h>
+﻿#include <bits/stdc++.h>
 using namespace std;
 
-const int mxN = 8, dx[4] = { 1, -1, 0, 0 }, dy[4] = { 0, 0, 1, -1 };
-int n, m, a[mxN][mxN];
-int tmp[mxN][mxN];
-int ans = 0;
-bool vis[mxN * mxN];
-vector<pair<int, int>> space;
+int moveX[4] = { 1, -1, 0, 0 };
+int moveY[4] = { 0, 0, 1, -1 };
 
-void copy(int tmp[mxN][mxN], int a[mxN][mxN]) {
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			tmp[i][j] = a[i][j];
-		}
-	}
+int M, N, K;
+int cnt;
+int arr[101][101];
+bool visited[101][101];
+vector<int> res;
+
+void dfs(int x, int y) {
+    int nx, ny;
+
+    visited[x][y] = true;
+    cnt++;
+
+    for (int i = 0; i < 4; i++) {
+        nx = x + moveX[i];
+        ny = y + moveY[i];
+
+        if (nx >= 0 && ny >= 0 && nx < M && ny < N && !visited[nx][ny]) {
+            dfs(nx, ny);
+        }
+    }
 }
 
-void bfs() {
+int main(void) {
+    scanf("%d %d %d", &M, &N, &K);
+    for (int i = 0; i < K; i++) {
+        int x1, x2, y1, y2;
+        scanf("%d %d %d %d", &x1, &y1, &x2, &y2);
+        for (int j = y1; j < y2; j++) {
+            for (int k = x1; k < x2; k++) {
+                arr[j][k] = 1;
+                visited[j][k] = true;
+            }
+        }
+    }
+    for (int i = 0; i < M; i++) {
+        for (int j = 0; j < N; j++) {
+            if (arr[i][j] == 0 && visited[i][j] == false) {
+                cnt = 0;
+                dfs(i, j);
+                res.push_back(cnt);
+            }
+        }
+    }
+    sort(res.begin(), res.end());
 
-	queue<pair<int, int>> q;
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			if (tmp[i][j] == 2) {
-				q.push({ i, j });
-			}
-		}
-	}
+    int size = res.size();
 
-	while (q.size()) {
-		int x = q.front().first;
-		int y = q.front().second;
-		q.pop();
+    printf("%d\n", size);
+    for (int i = 0; i < size; i++)
+        printf("%d ", res[i]);
 
-		for (int k = 0; k < 4; k++) {
-			int nx = x + dx[k];
-			int ny = y + dy[k];
-
-			if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-			if (tmp[nx][ny] == 0) {
-				tmp[nx][ny] = 2;
-				q.push({ nx, ny });
-			}
-		}
-	}
-
-	int cnt = 0;
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			if (tmp[i][j] == 0) {
-				cnt++;
-			}
-		}
-	}
-	ans = max(ans, cnt);
-}
-
-void wall(int cur, int idx) {
-
-	if (cur == 3) {
-		copy(tmp, a);
-		int cnt = 0;
-		for (int i = 0; i < space.size(); i++) {
-			if (cnt == 3) break;
-			if (vis[i]) {
-				int x = space[i].first;
-				int y = space[i].second;
-				tmp[x][y] = 1;
-				cnt++;
-			}
-		}
-		bfs();
-		return;
-	}
-
-	for (int i = idx; i < space.size(); i++) {
-		if (vis[i]) continue;
-		vis[i] = true;
-		wall(cur + 1, i);
-		vis[i] = false;
-	}
-}
-
-int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(0);
-	cout.tie(0);
-
-	cin >> n >> m;
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			cin >> a[i][j];
-			if (a[i][j] == 0) {
-				space.push_back({ i, j });
-			}
-		}
-	}
-
-	wall(0, 0);
-
-	cout << ans;
+    return 0;
 }

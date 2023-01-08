49e198bd4c0d39be558ee400e84b487b9d404fc0
Yaeeun Won <wyeyc12@gmail.com>
2023-01-08 22:48:04 +0900

    f

--- codepractice/codepractice.c
+++ codepractice/codepractice.c
@@ -36,7 +36,7 @@ int main(void) {
                 arr[j][k] = 1;
                 visited[j][k] = true;
             }
-        }
+        }dd
     }ssss
     for (int i = 0; i < M; i++) {
         for (int j = 0; j < N; j++) {ddd
